ec5808e72d5c21716edca73a30b6a6adba81fe2f
yubo <devinbo@163.com>
2024-05-22 13:48:22 +0800

    设置登录应答增加条件

--- Source/RtDstClient.cpp
+++ Source/RtDstClient.cpp
@@ -243,7 +243,7 @@ bool DstClient::UpdateLoginRspAndSendBack(Buffer *buff, const muduo::net::TcpCon
         // 3. 更新包数据
         GatePBStep stepLoginRsp;
         stepLoginRsp.SetPackage(rspMsg);
-        if (stepLoginRsp.FunctionId() == 6011)
+        if (stepLoginRsp.FunctionId() == 6011 && ::Successed(static_cast<GateError>(stepLoginRsp.GetBaseFieldValueInt(STEP_CODE))))
         {
             assert(stepLoginRsp.SetFieldValue(STEP_LAST_LOGIN_IP, m_LastIp.data()));
             assert(stepLoginRsp.SetFieldValue(STEP_LAST_LOGIN_MAC, m_LastMac.data()));
